c1597e6445bdcce00ed5e68866b9bd10bfde1f85
rudrakpatra <rudrakpatracode@gmail.com>
2024-09-18 20:12:05 +0530

    added tailwind prettier plugin

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -2,11 +2,10 @@
 	import Faucet from '$lib/components/Faucet.svelte';
 </script>
 
-<div class="mx-auto -mt-32 h-full pt-16">
+<main class="mx-auto -mt-32 h-full pt-16">
 	<div class="flex h-full w-full items-center justify-center pt-16">
-		<div class="flex basis-4/12 flex-col items-center justify-center 2xl:basis-3/12">
+		<div class="m-6 flex basis-4/12 flex-col items-center justify-center 2xl:basis-3/12">
 			<Faucet />
-			<a href="/page">Go to page</a>
 		</div>
 	</div>
-</div>
+</main>
